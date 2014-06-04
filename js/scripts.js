2e1cfa13e1c7002611755e14034f856d66b97898
Jordan Haines <jordan@jordanairwave.co.uk>
2014-06-04 11:27:25 +0100

    Pagination code added.  Tested on Chrome.  It is not responsive at the moment.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 var site = {
 	init: function() {
-
+	
+		//Pageination
+		$('ul.page-numbers a').on('click', function(event){
+			//Prevent Default Link action
+			event.preventDefault();
+			
+			//Get Page name from data-page attribute
+			var pageName = $(this).attr('data-page');
+			
+			//Add current class to parent li element and remove from others
+			$(this).parent().addClass('current').siblings().removeClass('current');
+			
+			//Show correct page and hide the rest.
+			$('#pagination-wrapper .page-wrapper #' + pageName).show().siblings().hide();
+		});
+		
     }
 };
 
